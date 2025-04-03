4e0761d2460cbf3708fd6fac63c2920f0182d792
jcifr99 <j.cifuentes.2021@alumnos.urjc.es>
2025-04-03 15:22:17 +0200

    Update ejercicio2_1.py

--- ejercicios/ejercicio2_1.py
+++ ejercicios/ejercicio2_1.py
@@ -11,18 +11,26 @@ app = Flask(__name__)
 def home():
     con = sqlite3.connect("incidentes.db")
     query_tickets = "SELECT * FROM tickets_emitidos"
-    query_contactos = "SELECT * FROM contactos_empleados"
+    #query_contactos = "SELECT * FROM contactos_empleados"
+    query_tipo_inc = "SELECT * FROM tickets_emitidos JOIN contactos_empleados ON tickets_emitidos.id_ticket_emitido = contactos_empleados.id_ticket_emitido"
     df_tickets = pd.read_sql_query(query_tickets, con)
-    df_contactos = pd.read_sql_query(query_contactos, con)
+    #df_contactos = pd.read_sql_query(query_contactos, con)
+    df_tipo_inc = pd.read_sql_query(query_tipo_inc, con)
 
     df_clientes_mas_incidencias = df_tickets.groupby('id_cliente')['tipo_incidencia'].count().reset_index(name='count').sort_values(by='count', ascending=False)
     result_clientes = df_clientes_mas_incidencias.head(5).apply(lambda x: f"{x['id_cliente']} - {x['count']}", axis=1).str.cat(sep=", ")
 
-    df_empleados_mas_tiempo = df_contactos.groupby('id_emp')['tiempo'].sum().reset_index(name='count').sort_values(by='count', ascending=False)
-    result_empleados = df_empleados_mas_tiempo.head(5).apply(lambda x: f"{x['id_emp']} - {x['count']}", axis=1).str.cat(sep=", ")
+    #df_empleados_mas_tiempo = df_contactos.groupby('id_emp')['tiempo'].sum().reset_index(name='count').sort_values(by='count', ascending=False)
+    #result_empleados = df_empleados_mas_tiempo.head(5).apply(lambda x: f"{x['id_emp']} - {x['count']}", axis=1).str.cat(sep=", ")
 
-    string_final = "Clientes con más incidencias (id - número de incidencias): " + result_clientes + "<br>Empleados con más tiempo (id - tiempo total): " + result_empleados
+    df_incidencia_mas_tiempo = df_tipo_inc.groupby('tipo_incidencia')['tiempo'].sum().reset_index(name='count').sort_values(by='count', ascending=False)
+    result_incidencias = df_incidencia_mas_tiempo.head(5).apply(lambda x: f"{x['tipo_incidencia']} - {x['count']}", axis=1).str.cat(sep=", ")
+
+    string_final = "Clientes con más incidencias (id - número de incidencias): " + result_clientes + "<br>Incidencias con más tiempo (id - tiempo total): " + result_incidencias
     return string_final
+
+
+
 
 
 if __name__ == '__main__':
