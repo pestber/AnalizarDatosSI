be2f2879cba6c4f99936d165cdde7c621c149959
jcifr99 <j.cifuentes.2021@alumnos.urjc.es>
2025-04-03 16:50:56 +0200

    Ejercicio 3 (contiene errores)

--- ejercicios/ejercicio2_1.py
+++ ejercicios/ejercicio2_1.py
@@ -3,51 +3,70 @@ import pandas as pd
 import numpy as np
 import json
 
-from flask import Flask, jsonify, request
+import requests
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-   #return "Bienvenido. Usa /top-clientes , /top-incidencias , /empleados para ver los datos."
     return '''
     <h1>Bienvenido</h1>
-    <p>Usa los siguientes enlaces para ver los datos:</p>
-    <ul>
-        <li><a href="/top-clientes">Top clientes con más incidencias</a></li>
-        <li><a href="/top-incidencias">Top tipo de incidencias con más tiempo</a></li>
-        <li><a href="/empleados">Empleados con más tiempo resolviendo incidencias</a></li>
-    </ul>
+    <h2>Top de clientes con más incidencias, tipo de incidencias con más tiempo o empleados con mas tiempo resolviendo incidencias</h2>
+    <p>Introduce el número de resultados que deseas ver:</p>
+    <form action="/resultados" method="get">
+        <label for="num_resultados">Número de resultados:</label>
+        <input type="number" id="num_resultados" name="num_resultados" min="1" required>
+        <input type="submit" value="Ver resultados">
+    </form>
+    
+    <h2>Top 10 vulnerabilidades</h2>
+    <li><a href="/top-vulnerabilidades">Top 10 vulnerabilidades basado a tiempo real</a></li>
     '''
+
+@app.route('/resultados')
+def resultados():
+    num_resultados = int(request.args.get('num_resultados', 5))
+    return '''
+    <h1>Resultados</h1>
+    <ul>
+        <li><a href="/top-clientes?num_resultados={0}">Top clientes con más incidencias</a></li>
+        <li><a href="/top-incidencias?num_resultados={0}">Top tipo de incidencias con más tiempo</a></li>
+        <li><a href="/empleados?num_resultados={0}">Empleados con más tiempo resolviendo incidencias</a></li>
+    </ul>
+    '''.format(num_resultados)
 
 
 @app.route('/top-clientes')
 def top_clientes():
+    num_resultados = int(request.args.get('num_resultados', 5))
     con = sqlite3.connect("incidentes.db")
     query_tickets = "SELECT * FROM tickets_emitidos"
     df_tickets = pd.read_sql_query(query_tickets, con)
     con.close()
     df_clientes_mas_incidencias = df_tickets.groupby('id_cliente')['tipo_incidencia'].count().reset_index(name='count').sort_values(by='count', ascending=False)
-    result_clientes = df_clientes_mas_incidencias.head(5).apply(lambda x: f"Id del cliente: {x['id_cliente']} - Número de incidencias: {x['count']}", axis=1).str.cat(sep="<br>")
+    result_clientes = df_clientes_mas_incidencias.head(num_resultados).apply(lambda x: f"Id del cliente: {x['id_cliente']} - Número de incidencias: {x['count']}", axis=1).str.cat(sep="<br>")
     return "Clientes con más incidencias (id - número de incidencias):<br>" + result_clientes + "<br>"
 
 
 @app.route('/top-incidencias')
 def top_incidencias():
+    num_resultados = int(request.args.get('num_resultados', 5))
     con = sqlite3.connect("incidentes.db")
     query_tipo_inc = "SELECT * FROM tickets_emitidos JOIN contactos_empleados ON tickets_emitidos.id_ticket_emitido = contactos_empleados.id_ticket_emitido"
     df_tipo_inc = pd.read_sql_query(query_tipo_inc, con)
     con.close()
 
     df_incidencia_mas_tiempo = df_tipo_inc.groupby('tipo_incidencia')['tiempo'].sum().reset_index(name='count').sort_values(by='count', ascending=False)
-    result_incidencias = df_incidencia_mas_tiempo.head(5).apply(lambda x: f"Tipo de incidencia: {x['tipo_incidencia']} - Tiempo total: {x['count']}", axis=1).str.cat(sep="<br>")
+    result_incidencias = df_incidencia_mas_tiempo.head(num_resultados).apply(lambda x: f"Tipo de incidencia: {x['tipo_incidencia']} - Tiempo total: {x['count']}", axis=1).str.cat(sep="<br>")
 
     return "Incidencias con más tiempo (id - tiempo total): <br>" + result_incidencias + "<br>"
 
 
 @app.route('/empleados')
-def empleados():
+def top_empleados():
+    num_resultados = int(request.args.get('num_resultados', 5))
     con = sqlite3.connect("incidentes.db")
     query_ticket = "SELECT * FROM tickets_emitidos JOIN contactos_empleados ON tickets_emitidos.id_ticket_emitido = contactos_empleados.id_ticket_emitido"
     query_emp = "SELECT * FROM empleados"
@@ -59,14 +78,23 @@ def empleados():
     df_empleados_mas_tiempo = df_empleados.groupby('id_emp')['tiempo'].sum().reset_index(name='count')
     df_empleados_mas_tiempo = df_empleados_mas_tiempo.merge(df_empleados.drop_duplicates(subset=['id_emp']), on='id_emp', how='left').sort_values(by='count', ascending=False)
 
-    result_empleados = df_empleados_mas_tiempo.head(30).apply(lambda x: f"Id de empleado: {x['id_emp']}, nombre: {x['nombre']}, nivel: {x['nivel']}, fecha de contrato: {x['fecha_contrato']}, número de incidencias: {x['count']}",  axis=1).str.cat(sep="<br>")
+    result_empleados = df_empleados_mas_tiempo.head(num_resultados).apply(lambda x: f"Id de empleado: {x['id_emp']}, nombre: {x['nombre']}, nivel: {x['nivel']}, fecha de contrato: {x['fecha_contrato']}, número de incidencias: {x['count']}",  axis=1).str.cat(sep="<br>")
 
     return "Empleados que más tiempo han empleado en resolución de incidentes:<br>" + result_empleados + "<br>"
 
 
-
-
-
+@app.route('/top-vulnerabilidades')
+def top_vulnerabilidades():
+    response = requests.get('https://cve.circl.lu/api/last')
+    if response.status_code == 200:
+        try:
+            vulnerabs = response.json()[:10]
+            result_vulnerabs = "<br>".join([f"CVE ID: {vuln.get('id', 'N/A')}, Summary: {vuln.get('summary', 'N/A')}" for vuln in vulnerabs])
+            return f"Top 10 vulnerabilidades basado a tiempo real:<br>{result_vulnerabs}"
+        except Exception as e:
+            return "Error al procesar los datos de vulnerabilidades: " + str(e)
+    else:
+        return "Error al obtener las vulnerabilidades."
 
 if __name__ == '__main__':
     app.run(debug=True)
