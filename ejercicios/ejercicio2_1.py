dd7c605fd32670e26e708312b4e94a81a1b175aa
jcifr99 <j.cifuentes.2021@alumnos.urjc.es>
2025-04-03 15:14:27 +0200

    Corrección ej1

--- ejercicios/ejercicio2_1.py
+++ ejercicios/ejercicio2_1.py
@@ -25,32 +25,6 @@ def home():
     return string_final
 
 
-
-
-@app.route('/top-clientes')
-def top_clientes():
-    con = sqlite3.connect("incidentes.db")
-    query_tickets = "SELECT * FROM tickets_emitidos"
-    df_tickets = pd.read_sql_query(query_tickets, con)
-    #seleccionamos del dataframe query_tickets los valores de id_cliente y los contamos y ordenamos segun el numero de incidencias de mayor a menor
-
-    #queremos obtener los clientes segun la cantidad de incidentes relacionada con ellos e imprimir esta cantidad
-    df_clientes_mas_incidencias = df_tickets.groupby('id_cliente')['tipo_incidencia'].count().reset_index(name='count').sort_values(by='count', ascending=False)
-    result = df_clientes_mas_incidencias.head(5).to_string(index=False, header=False)
-    return "Clientes con más incidencias" + result
-
-@app.route('/top-empleados')
-def top_empleados():
-    con = sqlite3.connect("incidentes.db")
-    query_contactos = "SELECT * FROM contactos_empleados"
-    df_contactos = pd.read_sql_query(query_contactos, con)
-    #seleccionamos del dataframe query_contactos los valores de id_emp y los del tiempo y ordenamos segun el id_emp que haya teido más tiempo en total ordenados de mayor a menor
-    df_empleados_mas_tiempo = df_contactos.groupby('id_emp')['tiempo'].sum().reset_index(name='count').sort_values(by='count', ascending=False)
-    result = df_empleados_mas_tiempo.head(5).to_string(index=False, header=False)
-    return "Empleados con más tiempo: " + result
-
-
-
 if __name__ == '__main__':
     app.run(debug=True)
 
